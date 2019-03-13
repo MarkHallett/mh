fc64c99d6a0971190aae91c6286c57314eba1937
Mark Hallett <markhallett@Marks-MacBook-Pro-2.local>
2019-03-13 22:36:54 +0000

    first commit

---
+++ mh/mh.py
@@ -0,0 +1,20 @@
+# eg.py
+'''
+eg code
+'''
+
+EG_VAR='ABCD'
+
+def testFunction():
+    ''' returns a test string '''
+    return 'OK'
+
+class Mh (object):
+    ''' Eg class '''
+
+    def runMh(self):
+        ''' Eg class function '''
+        print ('OK RUN')
+        return 100
+
+
