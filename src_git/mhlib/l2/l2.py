e253d26d86bf94ac823b9747459fbeec68453620
MarkHallett <git@markhallett.co.uk>
2022-05-12 22:58:57 +0100

    add comments to l2.py

---
+++ src_git/mhlib/l2/l2.py
@@ -0,0 +1,29 @@
+# l2.py
+'''
+l2 code
+'''
+
+import time
+
+L2EG_VAR='efgh'
+
+def L2Test():
+    ''' returns a test string '''
+    return 'ok'
+
+class L2Mh (object):
+    """Example l2 class """
+
+    def run(self):
+        "Eg function""
+        return 10
+
+    def slow(self):
+        "Eg function""
+        t = 5
+        time.sleep(t)
+        return t
+
+
+
+
