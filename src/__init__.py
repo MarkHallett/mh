d9152388ec170ea21d3687578b59f727b9236129
MarkHallett <git@markhallett.co.uk>
2022-05-13 22:23:21 +0100

    init files updated

--- src/__init__.py
+++ src/__init__.py
@@ -1,19 +1,15 @@
 # mh/__ini__.py
 '''
-eg code to start a project with.
-import mh
-To see use mh.__doc__
-Can also be seen with help(mh)
-And dir(mh)
-and mh.__version__
+This documentation comes from within the code.
+
+Deffinition of the API
 '''
 
-__version__ = '0.0.29'
+__version__ = '0.0.30'
 
 import mh.mh as mh
 
-# define mh api
-
-EG_VAR2=mh.EG_VAR
+EG_VAR2 = mh.EG_VAR
 testFunction = mh.testFunction
 Mh = mh.Mh
+
--- src/mh/__init__.py
+++ src/mh/__init__.py
@@ -1,15 +1,5 @@
 # mh/__ini__.py
 '''
-This documentation comes from within the code.
-
-Deffinition of the API
+documentation regarding the mh package...
 '''
-
-__version__ = '0.0.30'
-
-import mh.mh as mh
-
-EG_VAR2 = mh.EG_VAR
-testFunction = mh.testFunction
-Mh = mh.Mh
 
