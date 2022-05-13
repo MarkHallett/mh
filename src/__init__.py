0be14875940f8d53003a430a322818f7e6a6669a
MarkHallett <git@markhallett.co.uk>
2022-05-13 23:42:12 +0100

    swap __init__s

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,4 @@
-# mh/__ini__.py
 '''
-This documentation comes from within the code.
-
-Deffinition of the API
+Documentation regarding the mh package...
 '''
-
-__version__ = '0.0.31'
-
-import mh.mh as mh
-
-EG_VAR2 = mh.EG_VAR
-testFunction = mh.testFunction
-Mh = mh.Mh
 
--- src/mh/__init__.py
+++ src/mh/__init__.py
@@ -1,5 +1,15 @@
 # mh/__ini__.py
 '''
-documentation regarding the mh package...
+This documentation comes from within the code.
+
+Deffinition of the API
 '''
+
+__version__ = '0.0.32'
+
+import mh.mh as mh
+
+EG_VAR2 = mh.EG_VAR
+testFunction = mh.testFunction
+Mh = mh.Mh
 
